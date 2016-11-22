7abe9f323cbcb810cc6bd43c6d14ba7faf3840b5
Pol CONIN <pconin@e3r11p10.42.fr>
2016-11-22 16:00:05 +0100

    /

--- php/mergeimg.php
+++ php/mergeimg.php
@@ -6,7 +6,6 @@
 		header ('Location: accueil.php');
 		exit();
 	}
-	header('content-type: image/png');
 	header ('Location: accueil.php');
 	function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){ 
 	// function patch for respecting alpha work find on http://php.net/manual/fr/function.imagecopymerge.php
@@ -20,41 +19,6 @@
 	if ($_FILES['fileToUpload']['name'])
 	{
 	   move_uploaded_file($_FILES['fileToUpload']['tmp_name'], '../tmp/tmp1.png');
-	}
-	// else if (isset($_POST['img']))
-	// {
-	// 	$img = $_POST['img'];//(isset($_POST["img"])) ? htmlentities($_POST["img"]) : NULL;
- //    	$img = str_replace('data:image/png;base64,', '', $img);
-	// 	$img = str_replace(' ', '+', $img);
-	// 	$data = base64_decode($img);
-	// 	$im = imagecreatefromstring($data);
-	// 	header("content-type: image/png");
-	// //	print $_POST['img'];
- //    	//file_put_contents('tmp/tmp1.png', $data);
-	// 	move_uploaded_file($_POST['img'], '../tmp/tmp1.png');
-
-	// 	imagepng($im, $filename);
- // 	}
-
- 	else if (isset($_POST['img']) && $_POST['img'] != "") {
-	 	echo "snapshot";
-		// get the content of the captured image from the webcam put it in a tmp img
-		list($type, $data) = explode(';', $_POST['img']);
-		list(, $data) = explode(',', $data);
-		$data = base64_decode($data);
-		echo $data;
-		file_put_contents( '../tmp/tmp1.png', $data);
-		// creat image from this temporary 
-		$im = imagecreatefrompng($data);
-		// get selected alpha
-		$alpha = imagecreatefrompng('../effects/griffes.png');
-			
-		imagecopymerge_alpha($im, $alpha, 0, 0, 0, 0, imagesx($alpha), imagesy($alpha), 100);
-		// Create file name and register the image in database
-		imagepng($im, $filename);
-		// free memory
-		imagedestroy($im);
-		//die();
 	}
 	else
 	{
@@ -77,7 +41,6 @@
 
 	imagecopymerge_alpha($im, $filter, 0, 0, 0, 0, imagesx($filter), imagesy($filter), 100);
 	//imagecopyresampled($im, $filter, 300, 225, 0, 0, imagesx($filter), imagesy($filter), imagesx($filter), imagesy($filter));
-	header ('Content-Type: image/png');
 	imagepng($im);
 	imagepng($im, $filename);
 	imagedestroy($im);
